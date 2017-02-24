da661c03e122666f16cddddc1c2f47d77569cc52
Shruti Priya <shrutipriya1808@gmail.com>
2017-02-24 09:32:22 -0800

    add test for empty database in client test file

---
+++ Tests/ClientTest.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class ClientTest : IDisposable
+  {
+    public ClientTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Client.DeleteAll();
+    }
+
+    //Test for empty database in the beginning
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Client.GetAll().Count;
+
+      //Assert
+      Assert.Equal(0, result);
+    }

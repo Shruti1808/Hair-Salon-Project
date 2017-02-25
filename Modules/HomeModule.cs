dffc651f75062013be1e67e140f40c1ba435b173
Shruti Priya <shrutipriya1808@gmail.com>
2017-02-24 16:13:31 -0800

    add form to index.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
@@ -9,28 +10,15 @@ namespace HairSalon
     public HomeModule()
     {
       Get["/"] = _ => {
-        return View["index.cshtml"];
-      };
-
-      Get["/clients"] = _ => {
-        List<Client> AllClients = Client.GetAll();
-        return View["clients.cshtml", AllClients];
-      };
-
-      Get["/stylists"] = _ => {
         List<Stylist> allStylists = Stylist.GetAll();
-        return View["stylists.cshtml", allStylists];
+        List<Client> allClients = Client.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("stylists", allStylists);
+        model.Add("clients", allClients);
+        return View["index.cshtml",model];
       };
 
-      Get["/stylists/{id}"] = parameters => {
-        var SelectedStylist = Stylist.Find(parameters.id);
-        Model.Add("stylist", SelectedStylist);
-        return View["stylists.cshtml",SelectedStylist];
-      };
 
-      Get["/stylists/new"] = _ => {
-        return View["stylists_edit.cshtml"];
-      };
 
     }
   }
